adcd8acf9ab6f0cd25b3123aead2e059da1faf6c
Cristian Leicht <cristian.leicht@hotmail.com>
2025-02-24 19:53:24 -0300

    Order, Instant, ISO 8601 Pt.

--- src/main/java/com/cristianleicht/curso/entities/Order.java
+++ src/main/java/com/cristianleicht/curso/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.cristianleicht.curso.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -23,21 +24,24 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant momento;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+
+	private Integer orderStatus;
 
 	public Order() {
 
 	}
 
-	public Order(Long id, Instant momento, User client) {
+	public Order(Long id, Instant momento, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
+		setOrderStatus(orderStatus);
 		this.momento = momento;
 		this.client = client;
 	}
@@ -64,6 +68,16 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
