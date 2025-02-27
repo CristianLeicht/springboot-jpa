0065a8dd22c8e18ebece6b064c1e708518b5fe2e
Cristian Leicht <cristian.leicht@hotmail.com>
2025-02-27 20:05:13 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/cristianleicht/curso/entities/Product.java
+++ src/main/java/com/cristianleicht/curso/entities/Product.java
@@ -9,6 +9,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -17,25 +21,26 @@ import jakarta.persistence.Transient;
 public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	//Don't use List, use SET to ensure a product is not in multiple categories
-	@Transient
+
+	// Don't use List, use SET to ensure a product is not in multiple categories
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
-	public Product () {
-		
+
+	public Product() {
+
 	}
 
-	//Don't add collection in constructors
+	// Don't add collection in constructors
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
@@ -84,11 +89,10 @@ public class Product implements Serializable {
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
-	
+
 	public Set<Category> getCategories() {
 		return categories;
 	}
-	
 
 	@Override
 	public int hashCode() {
@@ -106,6 +110,5 @@ public class Product implements Serializable {
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-
 
 }
